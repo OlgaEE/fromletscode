c219bf1426b899b36f86fdf72cc3858f26c48ee1
Olga Titova <olgtit@hot.ee>
2020-08-15 13:14:51 +0300

    Starting, connect with DB, adding messages,searching by tag

---
+++ src/main/java/eu/comweb/letscode/domen/Message.java
@@ -0,0 +1,28 @@
+package eu.comweb.letscode.domen;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter
+@Entity //to have to save in DB
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+    public Message(String text, String tag) {
+        this.text  = text;
+        this.tag = tag;
+    }
+
+}
